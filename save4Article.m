6d5f9b8a64e8ba48d0590c8160c8ba489439a848
Piotr Wawryka <psw@agh.edu.pl>
2024-02-06 19:52:52 +0100

    Generate figures for publication script

---
+++ save4Article.m
@@ -0,0 +1,51 @@
+sourcePath = "figBaseNEW/"; 
+exportPath = "fig4Article/";
+% załaduj z pliku zamiast liczyć
+figFilenames = dir(strcat(sourcePath,"*.fig"));
+
+cetroidsFigNr = 0;
+fig527 = 0;
+stylL = 0;
+skipUpTo = 5;
+if(~isempty(figFilenames)) % jeśli w katalogu są pliki *.fig
+    close all force
+    for (i = 1:length(figFilenames))        
+        if(i<=skipUpTo) figure(i); continue; end
+        [filepath,name,ext] = fileparts(figFilenames(i).name);
+        pos = find(name == '_', 1, 'last');
+        a = openfig(strcat(figFilenames(i).folder,"/",figFilenames(i).name));
+        if(strfind(figFilenames(i).name,"1023")) cetroidsFigNr = i; end
+        if(strfind(figFilenames(i).name,"503")) fig527 = i; end
+        if(strfind(figFilenames(i).name,"555")) stylL = i; end
+        % if(i==4) break; end
+    end
+    backup = 0;
+else
+    fprintf(1,"In folder '%s' any *.fig file not found\n", sourcePath)
+end
+
+figHandles = findobj('Type', 'figure');
+num = [];
+for(i = 1:length(figHandles))
+    num = [num; figHandles(i).Number];
+end
+num = sort(num); % figNr
+
+toSave = [];
+for(i = 1:length(num))
+    toSave = [toSave; num(i)];
+end
+
+for (i = toSave')
+    if(i<=skipUpTo) continue;  end % for crashing matlab problem and reopen
+    figure(i);
+    
+    mnoznik = 1;
+    style = "stylePiotr";
+    if( (i == cetroidsFigNr) ) mnoznik = 1.6; end
+    if(i == fig527) mnoznik = 2.5;  end;
+    if(i == stylL) style = "styleLudwin"; end
+    % refresh
+    figPW("path", exportPath, "exportPDF", 1, "openFolder", 1, "TNR", 0, ...
+        "saveCopyFig", 0, "skipSaveAs", 1, "scale", mnoznik, style, 1);
+end
